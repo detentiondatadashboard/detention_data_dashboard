13e6968e98d89358f2233515ef867f97f1126be9
Lucas Olson <lucaskolson@Lucass-MacBook-Air.local>
2021-12-06 14:09:31 -0800

    updated __init__ files and added basic syntax for test file

---
+++ detention_data_dashboard/tests/__init__.py
@@ -0,0 +1,5 @@
+"""
+import all the functions from test_core script 
+"""
+
+from .test_core import *
---
+++ detention_data_dashboard/tests/test_core.py
@@ -0,0 +1,38 @@
+"""
+Tests for the data_detention_dashboard
+
+Includes one smoke test,  one-shot test, and edge test
+"""
+
+import unittest
+import numpy as np
+
+from .app import *
+
+
+class TestEntropy(unittest.TestCase):
+    """
+    create class for unittests
+    """
+
+    @classmethod
+    def test_smoke(cls):
+        """
+        Simple smoke test to make sure function runs.
+        """
+
+    def test_edge(self):
+        """
+        Edge test
+        """
+
+    @classmethod
+    def test_one_shot(cls):
+        """
+        One shot test
+        """
+
+
+
+
+
