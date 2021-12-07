e7ead9e8b890cc849d49a7004891988cb4851e12
Lucas Olson <lucaskolson@Lucass-MacBook-Air.local>
2021-12-06 19:29:26 -0800

    updated test file to list basic test cases

--- detention_data_dashboard/tests/test_core.py
+++ detention_data_dashboard/tests/test_core.py
@@ -16,23 +16,34 @@ class TestEntropy(unittest.TestCase):
     """
 
     @classmethod
-    def test_smoke(cls):
+    def test_smoke_figure(cls):
         """
-        Simple smoke test to make sure function runs.
+        Simple smoke test to make sure figure appears.
         """
 
-    def test_edge(self):
+    def test_edge_figure(self):
         """
-        Edge test
+        Edge test to make sure figure will not display if given bad inputs
         """
 
     @classmethod
-    def test_one_shot(cls):
+    def test_oneshot_figure(cls):
         """
-        One shot test
+        One shot test for figure by using fuzzy comparison of figure with known results
         """
 
+    def test_smoke_dataself):
+        """
+        Simple smoke test to make sure data downloads.
+        """
 
+    def test_edge_dataself):
+        """
+        Edge test to make sure data  will not download if given bad inputs
+        """
 
-
-
+    @classmethod
+    def test_oneshot_data(cls):
+        """
+        One shot test for data to make sure download has the correct information included (eg correct number of rows and columns)
+        """
