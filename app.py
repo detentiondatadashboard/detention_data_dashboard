78841a781d0171a45ae24f48505f31ecc58be1f9
Maddie Gaumer <mgaumer@g.hmc.edu>
2021-12-06 22:16:22 -0800

    updating app.py and exploration notebooks

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dash import html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
+from dash import callback_context
 
 df = px.data.election()
 geojson = px.data.election_geojson()
@@ -25,7 +26,8 @@ app.layout = html.Div([
     ),
     dcc.Graph(id="choropleth"),
     html.Button("Download CSV", id="btn_csv"),
-    dcc.Download(id="download-dataframe-csv"),    
+    dcc.Download(id="download"),
+    html.Button("Download Figure", id="btn_fig")
 ])
 
 
@@ -45,14 +47,20 @@ def display_choropleth(candidate):
 
 
 @app.callback(
-    Output("download-dataframe-csv", "data"),
+    Output("download", "data"),
     Input("btn_csv", "n_clicks"),
+    Input("btn_fig", "n_clicks"),
     prevent_initial_call=True,
 )
-def func(n_clicks):
-    return dcc.send_data_frame(df_csv.to_csv, "mydf.csv")
-
-
+def download(btn_csv, btn_fig):
+    changed_id = [p['prop_id'] for p in callback_context.triggered][0]
+    if 'btn_csv' in changed_id:
+        return dcc.send_data_frame(df_csv.to_csv, "mydf.csv")
+    elif 'btn_fig' in changed_id:
+        return dcc.send_file("./plot_downloads/test.png")
+    else:
+        msg = "Click Download!"
+        return html.Div(msg)
 
 
 if __name__ == "__main__":
