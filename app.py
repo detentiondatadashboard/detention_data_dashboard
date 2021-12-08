8306a2d3a03586c9aeb757e1c8c26cd24460be6a
Trung-Anh Nguyen <trunganh1410@gmail.com>
2021-12-08 02:09:44 -0800

    update dashboard look and css style sheet

--- app.py
+++ app.py
@@ -23,28 +23,53 @@ fy = ['2015-10-01', '2016-10-01', '2017-10-01', '2018-10-01']
 
 loc = ["East Coast", "West Coast", "Southwest", "Midwest", "All"]
 
-app.layout = html.Div([
-    html.P("Candidate:"),
-    dcc.RadioItems(
-        id='candidate', 
-        options=[{'value': x, 'label': x} 
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.H1(
+                    children="ICE Detention Analytics", className="header-title"
+                ),
+                html.P(
+                    children="A dashboard and data repository of"
+                    " ICE detention trends and facilities across the US"
+                    " between 2010 and 2020",
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        html.P("Candidate:"),
+        dcc.RadioItems(
+            id='candidate', 
+            options=[{'value': x, 'label': x} 
                  for x in candidates],
-        value=candidates[0],
-        labelStyle={'display': 'inline-block'}
-    ),
-    dcc.Graph(id="choropleth"),
-    html.Button("Download CSV", id="btn_csv"),
-    dcc.Download(id="download"),
-    html.Button("Download Figure", id="btn_fig"),
-    dcc.RadioItems(
-        id='us_loc', 
-        options=[{'value': x, 'label': x} 
-                 for x in loc],
-        value=loc[0],
-        labelStyle={'display': 'inline-block'}
-    ),
-    dcc.Graph(id="fy_arrests")
-])
+            value=candidates[0],
+            labelStyle={'display': 'inline-block'}
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[dcc.Graph(
+                        id="choropleth", config={"displayModeBar": False},
+                    ),
+                    html.Button("Download CSV", id="btn_csv"),
+                    dcc.Download(id="download-dataframe-csv"),
+                    html.Button("Download Image", id="btn_image"),
+                    dcc.Download(id="download-image")],
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="fy_arrests", config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
+    ]
+)
 
 
 @app.callback(
@@ -61,22 +86,23 @@ def display_choropleth(candidate):
 
     return fig
 
-
 @app.callback(
-    Output("download", "data"),
+    Output("download-dataframe-csv", "data"),
     Input("btn_csv", "n_clicks"),
-    Input("btn_fig", "n_clicks"),
     prevent_initial_call=True,
 )
-def download(btn_csv, btn_fig):
-    changed_id = [p['prop_id'] for p in callback_context.triggered][0]
-    if 'btn_csv' in changed_id:
-        return dcc.send_data_frame(df_csv.to_csv, "mydf.csv")
-    elif 'btn_fig' in changed_id:
-        return dcc.send_file("./plot_downloads/test.png")
-    else:
-        msg = "Click Download!"
-        return html.Div(msg)
+def func(n_clicks):
+    return dcc.send_data_frame(df_csv.to_csv, "mydf.csv")
+
+@app.callback(
+    Output("download-image", "data"),
+    Input("btn_image", "n_clicks"),
+    prevent_initial_call=True,
+)
+def func(n_clicks):
+    return dcc.send_file(
+        "./plot_downloads/test.png"
+    )
 
 @app.callback(
     Output("fy_arrests", "figure"),
