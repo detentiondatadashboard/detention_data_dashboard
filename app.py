07019ce000d6979391522cfcc08d9fc57bcd679a
Trung-Anh Nguyen <trunganh1410@gmail.com>
2021-12-09 22:48:06 -0800

    testing heroku

--- app.py
+++ app.py
@@ -10,37 +10,70 @@ df = px.data.election()
 geojson = px.data.election_geojson()
 candidates = df.winner.unique()
 
-app = dash.Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-df_csv = df
+app.title = "ICE Detention Data Dashboard"
 
 fy = ['2015-10-01', '2016-10-01', '2017-10-01', '2018-10-01']
 
 loc = ["East Coast", "West Coast", "Southwest", "Midwest", "All"]
 
-app.layout = html.Div([
-    html.P("Candidate:"),
-    dcc.RadioItems(
-        id='candidate', 
-        options=[{'value': x, 'label': x} 
-                 for x in candidates],
-        value=candidates[0],
-        labelStyle={'display': 'inline-block'}
-    ),
-    dcc.Graph(id="choropleth"),
-    html.Button("Download CSV", id="btn_csv"),
-    dcc.Download(id="download"),
-    html.Button("Download Figure", id="btn_fig"),
-    dcc.RadioItems(
-        id='us_loc', 
-        options=[{'value': x, 'label': x} 
-                 for x in loc],
-        value=loc[0],
-        labelStyle={'display': 'inline-block'}
-    ),
-    dcc.Graph(id="fy_arrests")
-])
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.H1(
+                    children="ICE Detention Analytics", className="header-title"
+                ),
+                html.P(
+                    children="A dashboard and data repository of"
+                    " ICE detention trends and facilities across the US"
+                    " between 2010 and 2020",
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        html.Div(
+            children=[
+                dcc.RadioItems(
+                id='candidate', 
+                options=[{'value': x, 'label': x} 
+                    for x in candidates],
+                value=candidates[0],
+                labelStyle={'display': 'inline-block'}
+                ),
+                html.Div(
+                    children=[dcc.Graph(
+                        id="choropleth", config={"displayModeBar": False},
+                    ),
+                    html.Button("Download CSV", id="btn_csv"),
+                    dcc.Download(id="download-dataframe-csv"),
+                    html.Button("Download Image", id="btn_image"),
+                    dcc.Download(id="download-image")],
+                    className="card",
+                ),
+                dcc.RadioItems(
+                    id='us_loc', 
+                    options=[{'value': x, 'label': x} 
+                            for x in loc],
+                    value=loc[0],
+                    labelStyle={'display': 'inline-block'}
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="fy_arrests",
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
+    ]
+)
 
 
 @app.callback(
@@ -57,27 +90,27 @@ def display_choropleth(candidate):
 
     return fig
 
-
 @app.callback(
-    Output("download", "data"),
+    Output("download-dataframe-csv", "data"),
     Input("btn_csv", "n_clicks"),
-    Input("btn_fig", "n_clicks"),
     prevent_initial_call=True,
 )
-def download(btn_csv, btn_fig):
-    changed_id = [p['prop_id'] for p in callback_context.triggered][0]
-    if 'btn_csv' in changed_id:
-        return dcc.send_data_frame(df_csv.to_csv, "mydf.csv")
-    elif 'btn_fig' in changed_id:
-        return dcc.send_file("./plot_downloads/test.png")
-    else:
-        msg = "Click Download!"
-        return html.Div(msg)
+def func(n_clicks):
+    return dcc.send_data_frame(df.to_csv, "mydf.csv")
+
+@app.callback(
+    Output("download-image", "data"),
+    Input("btn_image", "n_clicks"),
+    prevent_initial_call=True,
+)
+def func(n_clicks):
+    return dcc.send_file(
+        "./plot_downloads/test.png"
+    )
 
 @app.callback(
     Output("fy_arrests", "figure"),
-    Input("us_loc", "value")
-)
+    [Input("us_loc", "value")])
 
 def display_arrest_fy(us_loc):
     arrests_by_fy = pd.read_csv("arrests_by_fy.csv")
@@ -87,14 +120,12 @@ def display_arrest_fy(us_loc):
         aor = ['ATL', 'BAL', 'BOS', 'BUF', 'DET',  'MIA', 'NEW', 'NOL', 'NYC', 'PHI', 'WAS', 'HQ']
     elif us_loc == "Midwest":
         aor = ['CHI', 'SPM']
-    elif us_loc == 'Southwest':
+    elif us_loc == "Southwest":
         aor = ['DAL', 'DEN', 'ELP', 'HOU', 'PHO',  'SLC', 'SNA']
     elif us_loc == "All":
         aor = ['ATL', 'BAL', 'BOS', 'BUF', 'CHI', 'DAL', 'DEN', 'DET', 'ELP', 'HOU', 'HQ', 'LOS', 'MIA', 'NEW', 'NOL','NYC', 'PHI', 'PHO', 'SEA', 'SFR', 'SLC', 'SNA', 'SND', 'SPM', 'WAS']
     else:
         aor = ['ATL', 'BAL', 'BOS', 'BUF', 'CHI', 'DAL', 'DEN', 'DET', 'ELP', 'HOU', 'HQ', 'LOS', 'MIA', 'NEW', 'NOL','NYC', 'PHI', 'PHO', 'SEA', 'SFR', 'SLC', 'SNA', 'SND', 'SPM', 'WAS']
-
-
 
     fig = px.line(arrests_by_fy, x=fy, 
               y=aor, 
@@ -109,3 +140,4 @@ def display_arrest_fy(us_loc):
 
 if __name__ == "__main__":
     app.run_server(debug=True)
+
